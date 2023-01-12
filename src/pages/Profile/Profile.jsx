2df01f62f60e923c67d1f0b81a237989140fba61
17u5h <102058870+17u5h@users.noreply.github.com>
2023-01-12 17:23:19 +0700

    Header enhanced and some fixes

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import style from './style.module.css';
-import Header from "../Main/Header/Header";
-import LogoPic from "../../components/Logo/LogoPic";
-import UiButton from "../../components/UI/UiButton/UiButton";
 import {user} from "../../constants/userData-stub";
 import UserData from "./UserData/UserData";
 import Ads from "../../components/Ads/Ads";
 import {ads} from "../../constants/ads-stub";
+import HeaderWithLogo from "../../components/HeaderWithLogo/HeaderWithLogo";
 
 const Profile = () => {
 	const name = user.firstName || 'Пользователь'
 
 	return (
 		<div className={style.container}>
-			<Header isAuth={true}/>
-			<div className={style.topBlock}>
-				<LogoPic/>
-				<UiButton>Вернуться на главную</UiButton>
-			</div>
+			<HeaderWithLogo isAuth={true} />
 			<h1 className={style.greetings}>{`Здравствуйте, ${name}!`}</h1>
 			<UserData/>
+			<p className={style.subtitle}>Мои товары</p>
 			<Ads ads={ads}/>
 		</div>
 	);
