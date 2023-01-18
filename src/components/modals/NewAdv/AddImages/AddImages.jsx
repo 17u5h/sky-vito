eb33c18550794f0f2e4a7246c805c5f448bbbcea
17u5h <102058870+17u5h@users.noreply.github.com>
2023-01-18 18:54:42 +0700

    some style enhancing

--- src/components/modals/NewAdv/AddImages/AddImages.jsx
+++ src/components/modals/NewAdv/AddImages/AddImages.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import style from './style.module.css'
 import ReactImageUploading from "react-images-uploading";
-import UiCloseButton from "../../../UI/UiCloseButton/UiCloseButton";
 import {BASE_URL} from "../../../../constants/BASE_URL";
+import UiCloseIcon from "../../../UI/UiCloseIcon/UiCloseIcon";
 
 const AddImages = () => {
 
@@ -14,16 +14,20 @@ const AddImages = () => {
 	}
 	const id = 23
 
+	const backgroundImage = (url) => {
+		if(url){
+			return {background: `#f0f0f0 url("${BASE_URL}/${id}/image") no-repeat center`}
+		}
+	}
+
 	return (
 		<ReactImageUploading multiple value={images} onChange={onChange} maxNumber={maxNumber} dataURLKey=''>
 			{({imageList, onImageUpload, onImageRemove}) => (
 				<div className={style.container}>
 					{imageList.map((image, index) => (
-						<div key={index} className={style.imageItem} onClick={onImageUpload} style={{background: `#f0f0f0 url("${BASE_URL}/${id}/image")`}}>
-							<UiCloseButton onClick={onImageRemove}/>
+						<div key={index} className={style.imageItem} onClick={onImageUpload} style={backgroundImage(image)}>
+							{image && <UiCloseIcon onClick={onImageRemove}/>}
 						</div>
-
-
 					))}
 				</div>
 			)
