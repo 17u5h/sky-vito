ff37a6e7ce8b6145611c029d0639b73736db414e
17u5h <102058870+17u5h@users.noreply.github.com>
2023-01-29 22:31:05 +0700

    watch my adv. fixed background fit

--- src/pages/AdvDescription/AdvImages/AdvImages.jsx
+++ src/pages/AdvDescription/AdvImages/AdvImages.jsx
@@ -4,20 +4,29 @@ import {API_URL} from "../../../http/interceptors";
 
 const AdvImages = ({images}) => {
 
-	while(images.length < 6){
+	while (images.length < 6) {
 		images.push('')
 	}
 	const smallImages = images.slice(1, 6)
 
 	const bigImg = images[0]
-	const bigImgBackground = {background: `#F0F0F0 url("${API_URL}/${bigImg}") no-repeat center`, backgroundSize: 'cover'}
+	const bigImgBackground = {
+		backgroundImage: `url("${API_URL}/${bigImg}")`,
+		backgroundSize: 'cover',
+		backgroundColor: '#F0F0F0',
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center'
+	}
 
 	return (
 		<div className={style.container}>
 			<div className={style.bigImg} style={bigImgBackground}/>
 			<div className={style.smallImagesContainer}>
-				{smallImages.map(el => (<div key={Math.random()*1000} className={style.smallImg}
-																		 style={{background: `#F0F0F0 url("${API_URL}/${el}") no-repeat center`, backgroundSize: 'cover'}}/>))}
+				{smallImages.map(el => (<div key={Math.random() * 1000} className={style.smallImg}
+																		 style={{
+																			 background: `#F0F0F0 url("${API_URL}/${el}") no-repeat center`,
+																			 backgroundSize: 'cover'
+																		 }}/>))}
 			</div>
 		</div>
 	);
